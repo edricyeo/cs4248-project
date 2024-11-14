cb712dc620678662ad335acd53dc513452adb046
Du Jiahua <96218749+edures888@users.noreply.github.com>
2024-11-14 09:26:43 +0000

    Update train.sh

--- train.sh
+++ train.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:a100-80:1
 #SBATCH --mem=32gb
-#SBATCH --time=2:59:00
+#SBATCH --time=15:59:00
 #SBATCH --output=/home/e/e0725981/logs/%j-slurm.out
 
 
@@ -17,13 +17,3 @@ conda activate nlp
 nvidia-smi
 
 python ~/cs4248-project/app.py
-#exit 0
-
-python ~/cs4248-project/inference.py
-
-index=0
-for answer_file in "answers"/*; do
-    python evaluate-v2.0.py data/dev-v1.1.json "$answer_file" --out-file "eval_${index}.json" --out-image-dir out_images --verbose
-    cat "eval_${index}.json"
-    ((index++))
-done
